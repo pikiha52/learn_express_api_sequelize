3cf2509d6d1e8bee71d40811c3e71b6cc3b6f15a
pikiha52 <pikiha52@gmail.com>
2022-09-21 09:46:51 +0700

    add:learn middleware verify token

---
+++ middleware/verify.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+const verifyToken = async (req, res, next) => {
+    
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, process.env.JWT_KEY,  (err) => {
+            if (err) {
+                return res.status(500).send({ auth: false, message: err })
+            }
+            next()
+        })
+    } else {
+        res.status(401).send({
+            auth: false,
+            message: 'Token required'
+        })
+    }
+}
+
+module.exports = { verifyToken }
